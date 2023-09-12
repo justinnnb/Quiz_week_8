1ad2f0868575f58e5363ed6fbbc2401c99cfe47b
Justin <orochitachi13@gmail.com>
2023-09-12 14:38:10 +1000

    updated climate_q1.py file

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,32 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+connection = sqlite3.connect('climate.db')
+
+cursor = connection.cursor()
+
 years = []
 co2 = []
 temp = []
+
+cursor.execute("select Year from climatedata")
+current = cursor.fetchall()
+for year in current:
+    years.append(year)
+
+cursor.execute("select CO2 from climatedata")
+current = cursor.fetchall()
+for item in current:
+    co2.append(item)
+
+cursor.execute("select Temperature from climatedata")
+current = cursor.fetchall()
+for item in current:
+    temp.append(item)
+
+
+connection.commit()
+connection.close()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -16,3 +40,4 @@ plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)") 
 plt.show() 
 plt.savefig("co2_temp_1.png") 
+
